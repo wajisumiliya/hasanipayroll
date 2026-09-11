625275b2ed2f393c31b85ff4b758ceec5d8cf231
Wajeeth Ali <mohamedwajeethuali@gmail.com>
2026-09-11 15:12:27 +0800

    build web

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1328246360" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4190316548" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
