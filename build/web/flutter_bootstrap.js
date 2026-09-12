218beae7dff9891c99e9f681b02cc5e970872abc
Wajeeth Ali <mohamedwajeethuali@gmail.com>
2026-09-12 13:00:47 +0800

    edit style update

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3025361761" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3347095724" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
